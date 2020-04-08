9b0b56aa9bba17c7f0aeffb5eb76f447c1fd45e9
Raydel Alonso <raydelalonsobaryolo@gmail.com>
2020-04-07 22:14:59 -0400

    Add solution to card game war

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -8,6 +8,26 @@
         rank ranks]
     [suit rank]))
 
-(defn play-round [player1-card player2-card])
+(defn rrank [r]
+  (.indexOf ranks r))
 
-(defn play-game [player1-cards player2-cards])
+(defn play-round [[_ rank1] [_ rank2]]
+  (cond (> (rrank rank1) (rrank rank2)) :player1
+        (< (rrank rank1) (rrank rank2)) :player2
+        :else :tie))
+
+(defn play-game [player1-cards player2-cards]
+  (loop [acc [] [card1 & deck1] player1-cards  [card2 & deck2] player2-cards]
+    (cond
+      (not card1) :player2
+      (not card2) :player1
+      :else
+      (let [result (play-round card1 card2)]
+        (cond
+          (= result :tie) (recur (concat acc [card1 card2] (take 3 deck1) (take 3 deck2))
+                                 (drop 3 deck1)
+                                 (drop 3 deck2))
+          (= result :player1) (recur [] (concat deck1 acc [card1 card2]) deck2)
+          (= result :player2) (recur []  deck1 (concat deck2 acc [card2 card1])))))))
+
+
