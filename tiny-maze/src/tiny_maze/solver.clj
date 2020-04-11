cc174dc986a1a5c4d8f51ccfa4426678d4ab901f
Raydel Alonso <raydelalonsobaryolo@gmail.com>
2020-04-10 23:11:27 -0400

    Add tiny-maze solver solution

--- tiny-maze/src/tiny_maze/solver.clj
+++ tiny-maze/src/tiny_maze/solver.clj
@@ -1,3 +1,25 @@
 (ns tiny-maze.solver)
 
-(defn solve-maze [maze])
+(def dirs [[0 1] [1 0] [0 -1] [-1 0]])
+
+(defn solve [maze [x y :as pos]]
+  (when-let [v (get-in maze pos)]
+    (let [nmaze (assoc-in maze pos :x)]
+      (cond
+        (= v :E) nmaze
+        (or (= 0 v) (= :S v))
+        (some identity (map (fn [[i j]] (solve nmaze [(+ x i) (+ y j)])) dirs))))))
+
+(defn solve-maze [maze]
+  (let [pos (some #(when (not= -1 (second %)) %)
+        (map-indexed
+         (fn [idx v] [idx (.indexOf v :S)]) maze))]
+    (solve maze pos)))
+
+
+
+
+
+
+
+
