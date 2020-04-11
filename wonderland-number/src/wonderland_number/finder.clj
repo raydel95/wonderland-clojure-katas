89fc7487dea22296494a6348d06d1705797df26e
Raydel Alonso <raydelalonsobaryolo@gmail.com>
2020-04-11 01:21:02 -0400

    Add wonderland number finder

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,9 @@
 (ns wonderland-number.finder)
 
+(defn valid? [n] (apply = (map (comp set str)
+                      (cons n (map #(* % n) (range 2 7))))))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (first (filter valid? (range 100000 200000))))
+
+(wonderland-number)
